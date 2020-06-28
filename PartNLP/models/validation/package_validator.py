11604693838be6ca6309df3bd17ec4a8080009d1
mostafa <mostafarahgouy@gmail.com>
2020-06-28 15:09:09 +0430

    [Add] check_install_resources

--- PartNLP/models/validation/package_validator.py
+++ PartNLP/models/validation/package_validator.py
@@ -4,6 +4,10 @@ This class validates all requirements should be passed by package.
 from PartNLP.models.helper.color import Color
 from PartNLP.models.validation.validator import Validator
 from PartNLP.models.helper.constants import NAME_OF_SUPPORTED_PACKAGES
+from pathlib import Path
+import stanza
+import json
+import os
 
 
 class PackageValidator(Validator):
@@ -11,7 +15,12 @@ class PackageValidator(Validator):
         super().__init__(config)
 
     def isvalid(self):
-        return self.is_name_valid()
+        success, message, val = True, '', None
+        success, message, val = self.is_name_valid()
+        if success:
+            return self.check_install_resources()
+        else:
+            return success, message, val
 
     def is_name_valid(self):
         # Check whether package selected.
@@ -25,11 +34,30 @@ class PackageValidator(Validator):
                           f'{NAME_OF_SUPPORTED_PACKAGES}', package
         return True, '', None
 
+    def check_install_resources(self):
+        if self.config['package'] == 'STANZA':
+            HOME_DIR = str(Path.home())
+            DEFAULT_MODEL_DIR = os.getenv('STANZA_RESOURCES_DIR', os.path.join(HOME_DIR, 'stanza_resources/'))
+            dir = DEFAULT_MODEL_DIR
+            # rename persian language to fa and use dir = dir + self.config['Language']
+            dir = dir + 'fa'
+            if not os.path.isdir(dir):
+                lang = self.config['Language']
+                return False, f'stanza needs {Color.HEADER}{lang} resource. do you want to install(y, n){Color.ENDC}', \
+                       'install_resource'
+        return True, '', None
+
     def prepare_input_value(self):
         self.config['package'] = self.config['package'].upper()
 
     def update_config_value(self, name, old_value, new_value):
-        self.config['package'] = new_value
+        if old_value == 'install_resource':
+            if new_value == 'y':
+                stanza.download('fr')
+            else:
+                raise Exception('you can not run this package without the resources')
+        else:
+            self.config['package'] = new_value
 
     def get_dependencies(self):
         return []
