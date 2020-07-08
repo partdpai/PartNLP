fc1181429fc0954b1da08c0d3e9f3fd75ce57c88
mostafa <mostafarahgouy@gmail.com>
2020-07-08 16:37:53 +0430

    [Add] SUPPORTED_PROCESSORS_FOR_PACKAGES

--- PartNLP/models/helper/constants.py
+++ PartNLP/models/helper/constants.py
@@ -8,11 +8,17 @@ from PartNLP.models.pre_processors.parsivar_preprocessor import PARSIVARPreproce
 NAME_TO_PACKAGE_DICT = {'HAZM': HAZM, 'PARSIVAR': PARSIVAR, 'STANZA': STANZA}
 
 NAME_TO_METHODS = {'NORMALIZE': lambda m: m.normalize(), 'S_TOKENIZE': lambda m: m.sent_tokenize(),
-                         'W_TOKENIZE': lambda m: m.word_tokenize(), 'STEM': lambda m: m.stem(),
-                         'LEMMATIZE': lambda m: m.lemmatize(), 'POS': lambda m: m.pos()}
+                   'W_TOKENIZE': lambda m: m.word_tokenize(), 'STEM': lambda m: m.stem(),
+                   'LEMMATIZE': lambda m: m.lemmatize(), 'POS': lambda m: m.pos()}
 
 SUPPORTED_LANGUAGES_TO_PACKAGES = {'PERSIAN': ['HAZM', 'PARSIVAR', 'STANZA'], 'ENGLISH': ['STANZA']}
-SUPPORTED_PROCESSORS = ['NORMALIZE', 'S_TOKENIZE', 'STEM', 'W_TOKENIZE', 'LEMMATIZE', 'POS']
+SUPPORTED_PROCESSORS_FOR_PACKAGES = {'HAZM': ['NORMALIZE', 'S_TOKENIZE', 'STEM', 'W_TOKENIZE', 'LEMMATIZE'],
+                                     'PARSIVAR': ['NORMALIZE', 'S_TOKENIZE', 'STEM', 'W_TOKENIZE'],
+                                     'STANZA': ['S_TOKENIZE', 'STEM', 'W_TOKENIZE', 'LEMMATIZE']
+                                     }
 NAME_OF_SUPPORTED_LANGUAGES = ['ENGLISH', 'PERSIAN']
 NAME_OF_SUPPORTED_PACKAGES = ['HAZM', 'PARSIVAR', 'STANZA']
 EQUIVALENT_LANGUAGES_TO_STANZA = {'ENGLISH': 'en', 'PERSIAN': 'fa'}
+
+PROCESSORS_TO_LABELS = {'S_TOKENIZE': 'Sent tokenize', 'STEM': 'Stem', 'W_TOKENIZE': 'Word tokenize',
+                        'NORMALIZE': 'Normalize', 'LEMMATIZE': 'Lemma'}
