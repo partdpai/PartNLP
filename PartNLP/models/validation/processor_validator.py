cd35a7714e2020c825e0aa77c48dae4b4942904d
mostafa <mostafarahgouy@gmail.com>
2020-07-08 16:45:45 +0430

    [Add] SUPPORTED_PROCESSORS dic

--- PartNLP/models/validation/processor_validator.py
+++ PartNLP/models/validation/processor_validator.py
@@ -1,26 +1,28 @@
 from PartNLP.models.validation.validator import Validator
-from PartNLP.models.helper.constants import SUPPORTED_PROCESSORS
+from PartNLP.models.helper.constants import SUPPORTED_PROCESSORS_FOR_PACKAGES
 from PartNLP.models.helper.color import Color
 
 
 class ProcessorsValidator(Validator):
     def __init__(self, config):
         super().__init__(config)
-    
+        self.supported_operations = SUPPORTED_PROCESSORS_FOR_PACKAGES[self.config['package']]
+
     def isvalid(self):
         return self.is_name_valid()
 
     def is_name_valid(self):
         self.prepare_input_value()
+
         # Check if processors is empty.
         if not self.config['processors']:
             return False, f'{Color.FAIL}Warning{Color.ENDC} No operator selected. List of supported operations:' \
-                          f'{Color.HEADER}{SUPPORTED_PROCESSORS}{Color.ENDC}', self.config['processors']
+                          f'{Color.HEADER}{self.supported_operations}{Color.ENDC}', self.config['processors']
         # Check if whether operators of the processor are supported or not.
         for p in self.config['processors']:
-            if p not in SUPPORTED_PROCESSORS:
+            if p not in self.supported_operations:
                 return False, f'{Color.FAIL}{p}{Color.ENDC} Operator is not supported. ' \
-                              f'List of supported operators : {Color.HEADER}{SUPPORTED_PROCESSORS}{Color.ENDC}', p
+                              f'List of supported operators : {Color.HEADER}{self.supported_operations}{Color.ENDC}', p
         return True, '', None
     
     def update_config_value(self, name, old_value, new_value):
