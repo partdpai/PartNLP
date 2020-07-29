2ee28c02d27ee499fcf0e417a37b43b22443d5af
mostafa <mostafarahgouy@gmail.com>
2020-07-29 13:40:41 +0430

    [Refactor] clean __init__ of PipeLine class

--- PartNLP/core.py
+++ PartNLP/core.py
@@ -7,8 +7,8 @@ import os
 import logging
 from PartNLP.models.helper import configuration
 from PartNLP.models.validation.config_validator import config_validator
-from PartNLP.models.helper.constants import NAME_TO_PACKAGE_DICT, NAME_TO_METHODS
-from PartNLP.models.helper.readers_and_writers.txt_reader_and_writer import TxtReader, TxtWriter
+from PartNLP.models.helper.constants import NAME_TO_PACKAGE_DICT, \
+    NAME_TO_METHODS, NAME_TO_READER_AND_WRITER
 from PartNLP.models.helper.readers_and_writers.reader_and_writer \
     import InputDocument, OutPutDocument
 from PartNLP.models.helper.utils import batch_data
@@ -41,21 +41,16 @@ class Pipeline:
 
         # >>> Pipeline(package='HAZM', text=text, processors=['S_TOKENIZE', 'W_TOKENIZE'])
      """
-
-    def __init__(self, lang='persian', package='HAZM',
-                 processors=[], text='', input_file='', **kwargs):
-        config = configuration.get_config()
-        config['processors'] = processors
-        output_list = config['processors']
-        config['package'] = package
-        config['InputFilePath'] = input_file
-        config['Language'] = lang
-        if config['InputFilePath'] != '':
-            self.document = TxtReader().read_data(
-                InputDocument(config['InputFilePath'], 'txt'))
-        # Check dependencies and validate config
+    def __init__(self, input_file_path, input_file_format, lang='persian', package='HAZM', processors=[], **kwargs):
+        self.output_list = []
+        self.document = []
+        config = self.__initialize_config(input_file_path, input_file_format,
+                                          lang, package, processors)
         config_validator(config)
-        self._work_flow(config, output_list)
+        self.reader_writer_obj = NAME_TO_READER_AND_WRITER[config['InputFileFormat']]()
+        self.document = self.reader_writer_obj.read_data(InputDocument(
+            config['InputFilePath'], config['InputFileFormat']))
+        self._work_flow(config, self.output_list)
 
     def _work_flow(self, config, output_list):
         processors, package = config['processors'], config['package']
@@ -67,5 +62,17 @@ class Pipeline:
             for operation in processors:
                 output_value = NAME_TO_METHODS[operation](model)
                 if operation in output_list:
-                    TxtWriter().write_data(OutPutDocument(output_value, operation, package))
+                    self.reader_writer_obj.write_data(
+                        OutPutDocument(output_value, operation, package))
         logging.info(f'the result has been saved in {os.getcwd()}/preprocessed folder')
+
+    def __initialize_config(self, input_file_path, input_file_format,
+                            lang='persian', package='HAZM', processors=[]):
+        config = configuration.get_config()
+        config['InputFileFormat'] = input_file_format
+        config['processors'] = processors
+        self.output_list = config['processors']
+        config['package'] = package
+        config['InputFilePath'] = input_file_path
+        config['Language'] = lang
+        return config
