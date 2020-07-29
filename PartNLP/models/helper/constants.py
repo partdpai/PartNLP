825f14735c570c4405381330b3f56720dc858922
mostafa <mostafarahgouy@gmail.com>
2020-07-29 13:39:40 +0430

    [Add] NAME_TO_READER_AND_WRITER dictionary

--- PartNLP/models/helper/constants.py
+++ PartNLP/models/helper/constants.py
@@ -4,12 +4,16 @@ In this script, we are going to add all constant values in which used in the ent
 from PartNLP.models.pre_processors.hazm_preprocessor import HAZMPreprocessor as HAZM
 from PartNLP.models.pre_processors.stanza_preprocessor import STANZAPreprocessor as STANZA
 from PartNLP.models.pre_processors.parsivar_preprocessor import PARSIVARPreprocessor as PARSIVAR
+from PartNLP.models.helper.readers_and_writers.txt_reader_and_writer import TxtReaderAndWriter
+from PartNLP.models.helper.readers_and_writers.json_reader_and_writer import JsonReaderAndWriter
 
 NAME_TO_PACKAGE_DICT = {'HAZM': HAZM, 'PARSIVAR': PARSIVAR, 'STANZA': STANZA}
 
 NAME_TO_METHODS = {'NORMALIZE': lambda m: m.normalize(), 'S_TOKENIZE': lambda m: m.sent_tokenize(),
                    'W_TOKENIZE': lambda m: m.word_tokenize(), 'STEM': lambda m: m.stem(),
                    'LEMMATIZE': lambda m: m.lemmatize(), 'POS': lambda m: m.pos()}
+
+NAME_TO_READER_AND_WRITER = {'TXT': TxtReaderAndWriter, 'JSON': JsonReaderAndWriter}
 
 SUPPORTED_LANGUAGES_TO_PACKAGES = {'PERSIAN': ['HAZM', 'PARSIVAR', 'STANZA'], 'ENGLISH': ['STANZA']}
 SUPPORTED_PROCESSORS_FOR_PACKAGES = {'HAZM': ['NORMALIZE', 'S_TOKENIZE', 'STEM', 'W_TOKENIZE', 'LEMMATIZE'],
