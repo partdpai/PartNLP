28c95706562cc3daddc3546b490880d0b71bc3c9
mostafa <mostafarahgouy@gmail.com>
2020-07-29 13:43:08 +0430

    [Refactor] merge Reader and Writer Classes into ReaderAndWriter

--- PartNLP/models/helper/readers_and_writers/txt_reader_and_writer.py
+++ PartNLP/models/helper/readers_and_writers/txt_reader_and_writer.py
@@ -6,15 +6,15 @@
 import os
 import logging
 from PartNLP.models.helper.readers_and_writers.reader_and_writer \
-    import Reader, Writer, Document, Types
+    import ReaderAndWriter, Document, Types
 
 
-class TxtReader(Reader):
+class TxtReaderAndWriter(ReaderAndWriter):
     """
             TXT READER
     """
     def __init__(self):
-        super(TxtReader, self).__init__()
+        super(TxtReaderAndWriter, self).__init__()
         self.docs = []
 
     def read_data(self, data):
@@ -22,19 +22,11 @@ class TxtReader(Reader):
         Args:
             data: An object of InputDocument
         Returns:
-            None
+            Document: An object
         """
         self.docs = open(data.path, encoding='utf-8', errors='ignore').read()
         self.docs = self.docs.split(data.separator)
         return Document(self.docs, Types.txt_type)
-
-
-class TxtWriter(Writer):
-    """
-        TXT WRITER
-    """
-    def __init__(self):
-        super(TxtWriter, self).__init__()
 
     def write_data(self, result_data):
         """
