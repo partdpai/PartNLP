6f9a98d366c34b4e53f52f4c4f468abd8373701d
mostafa <mostafarahgouy@gmail.com>
2020-08-27 13:08:52 +0430

    [Refactor] core add multiprocess property add program profile refactor clean codes

--- PartNLP/core.py
+++ PartNLP/core.py
@@ -5,14 +5,16 @@
 """
 import os
 import logging
+import concurrent.futures
+from time import perf_counter
 from tqdm import tqdm
 from PartNLP.models.helper import configuration
+from PartNLP.models.helper.program_profiling import show_program_profile
 from PartNLP.models.validation.config_validator import config_validator
 from PartNLP.models.helper.constants import NAME_TO_PACKAGE_DICT, \
-    NAME_TO_METHODS, NAME_TO_READER_AND_WRITER
+    NAME_TO_METHODS, NAME_TO_READER_AND_WRITER, BATCH_SIZE_BASED_ON_PACKAGE
 from PartNLP.models.helper.readers_and_writers.reader_and_writer \
     import InputDocument, OutPutDocument
-from PartNLP.models.helper.time_and_usage_profiling import profile
 
 
 class Pipeline:
@@ -42,38 +44,68 @@ class Pipeline:
 
         # >>> Pipeline(package='HAZM', text=text, processors=['S_TOKENIZE', 'W_TOKENIZE'])
      """
-    def __init__(self, input_file_path, input_file_format, lang='persian',
+    def __init__(self, input_file_path, use_multiprocess=True, lang='persian',
                  package='HAZM', processors=[], **kwargs):
-        self.output_list, self.document = [], []
-        config = self.__initialize_config(input_file_path, input_file_format,
-                                          lang, package, processors)
+        self.output_list = []
+        config = self.__initialize_config(input_file_path, lang,
+                                          package, processors,
+                                          use_multiprocess, **kwargs)
         config_validator(config)
         self.reader_writer_obj = NAME_TO_READER_AND_WRITER[config['InputFileFormat']]()
         self._work_flow(config, self.output_list)
 
-    @profile
-    def _work_flow(self, config, output_list):
-        processors, package = config['processors'], config['package']
-        # Execute selected operator by calling its corresponded method
+    def _work_flow(self, config, selected_operations):
+        start_time = perf_counter()
         os.makedirs(os.getcwd() + '/preprocessed', exist_ok=True)
         data = InputDocument(config['InputFilePath'], config['InputFileFormat'])
-        for lines in tqdm(self.reader_writer_obj.read_data(data, batch_size=10000000)):
-            config['text'] = '\n'.join(lines)
-            model = NAME_TO_PACKAGE_DICT[package](config)
-            for operation in processors:
-                output_value = NAME_TO_METHODS[operation](model)
-                if operation in output_list:
-                    self.reader_writer_obj.write_data(
-                        OutPutDocument(output_value, operation, package))
+        self._check_multiprocess(config, selected_operations, data)
         logging.info(f'the result has been saved in {os.getcwd()}/preprocessed folder')
+        program_time = perf_counter() - start_time
+        show_program_profile(config['package'], self.batch_size,
+                             config['processors'], program_time,
+                             config['use_multiprocess'],
+                             self.reader_writer_obj.get_file_size(config['InputFilePath']))
 
-    def __initialize_config(self, input_file_path, input_file_format,
-                            lang, package, processors):
+    def _check_multiprocess(self, config, selected_operations, data):
+        if config['use_multiprocess']:
+            self._run_with_multiprocessing(config, selected_operations, data)
+        else:
+            self._run_without_multiprocessing(config, selected_operations, data)
+
+    def _run_with_multiprocessing(self, config, output_list, data):
+        with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
+            for index, batch_text in enumerate(
+                    tqdm(self.reader_writer_obj.read_data(data, batch_size=self.batch_size))):
+                executor.submit(
+                    self._run, config, config['processors'],
+                    output_list, config['package'], batch_text, index)
+
+    def _run_without_multiprocessing(self, config, output_list, data):
+        for index, batch_text in enumerate(
+                tqdm(self.reader_writer_obj.read_data(data, batch_size=self.batch_size))):
+            self._run(config, config['processors'],
+                      output_list, config['package'], batch_text, index)
+
+    def _run(self, *args):
+        args[0]['text'] = '\n'.join(args[4])
+        model = NAME_TO_PACKAGE_DICT[args[3]](args[0])
+        for operation in args[1]:
+            self._run_operation(model, operation, args[2], args[3], args[5])
+
+    def _run_operation(self, *args):
+        output_value = NAME_TO_METHODS[args[1]](args[0])
+        if args[1] in args[2]:
+            self.reader_writer_obj.write_data(
+                OutPutDocument(output_value, args[1], args[3]))
+
+    def __initialize_config(self, input_file_path, lang, package,
+                            processors, use_multiprocess, **kwargs):
         config = configuration.get_config()
-        config['InputFileFormat'] = input_file_format
-        config['processors'] = processors
-        self.output_list = config['processors']
-        config['package'] = package
-        config['InputFilePath'] = input_file_path
-        config['Language'] = lang
+        config['processors'], config['use_multiprocess'] = processors, use_multiprocess
+        self.output_list, config['package'] = config['processors'], package
+        config['InputFilePath'], config['Language'] = input_file_path, lang
+        if 'batch_size' in kwargs.keys():
+            self.batch_size = kwargs['batch_size']
+        else:
+            self.batch_size = BATCH_SIZE_BASED_ON_PACKAGE[config['package']]
         return config
