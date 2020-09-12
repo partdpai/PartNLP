9b1c9af151bba5d9fb3a45a2e833555eb56424b1
mostafa <mostafarahgouy@gmail.com>
2020-09-12 10:40:20 +0430

    [Refactor] - add more stylistic output for each operation

--- PartNLP/models/helper/readers_and_writers/txt_reader_and_writer.py
+++ PartNLP/models/helper/readers_and_writers/txt_reader_and_writer.py
@@ -16,7 +16,8 @@ class TxtReaderAndWriter(ReaderAndWriter):
     """
     def __init__(self):
         super(TxtReaderAndWriter, self).__init__()
-        self.docs = []
+        self.docs, self.next_document = [], False
+        self.splitter = '===================next paragraph==================='
 
     def read_data(self, data, batch_size):
         """
@@ -41,10 +42,66 @@ class TxtReaderAndWriter(ReaderAndWriter):
         """
         with open(os.getcwd() + '/preprocessed' + '/' +
                   result_data.package + '.' + result_data.operation + '.txt', 'a') as outfile:
-            for value in result_data.output_value:
-                outfile.write(str(value) + '\n') if value else outfile.write('\n')
-            outfile.close()
+            self._write_structured_data_base_operation(result_data, outfile)
         logging.getLogger().setLevel(logging.INFO)
+
+    def _write_structured_data_base_operation(self, result_data, outfile):
+        """
+        Args:
+            result_data:
+            outfile:
+        Returns:
+        """
+        if result_data.operation == 'normalize':
+            self._write_paragraph_level(result_data.output_value, outfile)
+        elif result_data.operation == 's_tokenize':
+            self._write_sentence_level(result_data.output_value, outfile)
+        else:
+            self._write_word_level(result_data.output_value, outfile)
+
+    def _write_paragraph_level(self, items, outfile):
+        """
+        Args:
+            items:
+            outfile:
+        Returns:
+        """
+        for item in items:
+            self.next_document = False
+            if item and item != '\n':
+                outfile.write(str(item) + '\n')
+                self.next_document = True
+            outfile.write(f'{self.splitter} \n') if self.next_document else None
+
+    def _write_sentence_level(self, chunks, outfile):
+        for chunk in chunks:
+            self.iterate_items(chunk, outfile)
+            outfile.write(f'{self.splitter} \n') if self.next_document else None
+
+    def _write_word_level(self, chunks, outfile):
+        """
+        Args:
+            chunks:
+            outfile:
+        Returns:
+        """
+        for chunk in chunks:
+            self.next_document = False
+            for words in chunk:
+                self.iterate_items(words, outfile)
+            outfile.write(f'{self.splitter} \n') if self.next_document else None
+
+    def iterate_items(self, items, outfile):
+        """
+        Args:
+            items:
+            outfile:
+        Returns:
+        """
+        self.next_document = False
+        for item in items:
+            outfile.write(str(item) + '\n') if item and item != '\n' else None
+            self.next_document = True
 
     def get_file_size(self, path):
         return Path(path).stat().st_size
